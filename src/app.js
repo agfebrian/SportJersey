23e313ac67708d7b4f96ddab8bc37065107bc240
agfebrian <54467106+agfebrian@users.noreply.github.com>
2021-08-15 13:53:41 +0700

    Update app.js
    clean comment

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import "regenerator-runtime";
 import "./styles/tailwind.css";
-// import "./script/component/app-bar.js";
 import main from "./script/view/main.js";
 
 document.addEventListener("DOMContentLoaded", main);
